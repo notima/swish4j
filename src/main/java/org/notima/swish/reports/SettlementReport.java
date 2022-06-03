866437675cf146ade7096091eac5cb820ef7bb3a
Daniel Tamm <daniel@notima.se>
2022-06-03 16:10:50 +0200

    Add helper methods for date determination.

--- src/main/java/org/notima/swish/reports/SettlementReport.java
+++ src/main/java/org/notima/swish/reports/SettlementReport.java
@@ -1,7 +1,16 @@
 package org.notima.swish.reports;
 
+import java.beans.Transient;
+import java.util.Date;
 import java.util.List;
 
+/**
+ * Representation of a Swish settlement report.
+ * 
+ * @author Daniel Tamm
+ * @author Oliver Norin
+ *
+ */
 public class SettlementReport {
     private String clearingNumber;
     private String accountNumber;
@@ -30,4 +39,43 @@ public class SettlementReport {
     public void setRows(List<SettlementReportRow> rows) {
         this.rows = rows;
     }
+    
+    @Transient
+    public boolean hasRows() {
+    	return rows!=null && !rows.isEmpty();
+    }
+    
+    @Transient
+    public Date getFirstTransactionDate() {
+    	if (!hasRows()) return null; 
+
+    	Date firstDate = new Date(Long.MAX_VALUE);
+    	
+    	for (SettlementReportRow r : rows) {
+    		if (r.getTransactionDate().before(firstDate)) {
+    				firstDate = r.getTransactionDate();
+    		}
+    	}
+    	
+    	return firstDate;
+    }
+    
+    @Transient
+    public Date getLastTransactionDate() {
+    	if (!hasRows()) return null; 
+
+    	Date lastDate = new Date(0);
+    	
+    	for (SettlementReportRow r : rows) {
+    		if (r.getTransactionDate().after(lastDate)) {
+    				lastDate = r.getTransactionDate();
+    		}
+    	}
+    	
+    	return lastDate;
+    }
+    
+    
+    
+    
 }
